92b4e7a2aefbb32f64d7378811cb2f9257b846bc
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2023-07-22 18:57:33 -0300

    adicionando ts e model

---
+++ components/Porta.tsx
@@ -0,0 +1,24 @@
+import styles from '../src/styles/Porta.module.css'
+import PortaModel from '../model/porta'
+
+interface PortaProps {
+    porta: PortaModel
+}
+
+export default function Porta(props: PortaProps) {
+
+    const { porta } = props
+    const selecionada = porta.selecionada ? styles.selecionada : ''
+
+    return (
+        <div className={styles.area}>
+            <div className={`${styles.frame} ${selecionada} `}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>{porta.numero}</div>
+                    <div className={styles.macaneta}></div>
+                </div>
+            </div>
+            <div className={styles.chao}></div>
+        </div>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import Porta from '../../components/Porta'
+import PortaModel from '../../model/porta'
 
 
 export default function Home() {
+  const [p1, setP1 ] = useState(new PortaModel(1))
+  
   return (
-    <>
-      <Porta />
-    </>
+    <div style={{display: 'flex'}}>
+      <Porta porta={p1}/>
+    </div>
   )
 }
